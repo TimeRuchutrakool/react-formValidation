16b060dd6474b8dd4e0931d3e657fc9cb89fb3a7
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-04 20:08:48 +0700

    update:formSubmit

--- src/index.js
+++ src/index.js
@@ -11,8 +11,15 @@ function Form() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  // Error State
+  // Error Message State
   const [errorUserName, setErrorUserName] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState('');
+
+  // Color State
+  const [usernameColor, setUserNameColor] = useState('');
+  const [emailColor, setEmailColor] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,8 +27,33 @@ function Form() {
     // Validate Username
     if (username.length >= 8) {
       setErrorUserName('');
+      setUserNameColor('green');
     } else {
       setErrorUserName('กรุณาระบุชื่อผู้ใช้งานอย่างน้อย 8 ตัวอักษร');
+      setUserNameColor('red');
+    }
+
+    // Validate Email
+    if (email.includes('@')) {
+      setErrorEmail('');
+      setEmailColor('green');
+    } else {
+      setErrorEmail('รูปแบบอีเมลล์ไม่ถูกต้อง');
+      setEmailColor('red');
+    }
+
+    // Validate Password
+    if (password.length >= 8) {
+      setErrorPassword('');
+    } else {
+      setErrorPassword('กรุณาระบุรหัสผ่านอย่างน้อย 8 ตัวอักษร');
+    }
+
+    // Validate Confirm Password
+    if (password === confirmPassword) {
+      setErrorConfirmPassword('');
+    } else {
+      setErrorConfirmPassword('รหัสผ่านไม่ตรงกัน กรุณาระบุใหม่');
     }
   };
   return (
@@ -30,22 +62,32 @@ function Form() {
         {/* #1 : Username */}
         <div className='form-input'>
           <label>username</label>
-          <input type='text' value={username} onChange={(e) => setUserName(e.target.value)} />
-          <small>{errorUserName}</small>
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUserName(e.target.value)}
+            style={{ borderColor: usernameColor }}
+          />
+          <small style={{ color: usernameColor }}>{errorUserName}</small>
         </div>
 
         {/* #2 : Email*/}
         <div className='form-input'>
           <label>email</label>
-          <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
-          <small>รูปแบบอีเมลล์ไม่ถูกต้อง</small>
+          <input
+            type='text'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ borderColor: emailColor }}
+          />
+          <small style={{ color: emailColor }}>{errorEmail}</small>
         </div>
 
         {/* #3 : Password */}
         <div className='form-input'>
           <label>password</label>
           <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-          <small>กรุณาระบุรหัสผ่านอย่างน้อย 8 ตัวอักษร</small>
+          <small>{errorPassword}</small>
         </div>
 
         {/* #4 : Confirm Password */}
@@ -56,7 +98,7 @@ function Form() {
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
-          <small>รหัสผ่านไม่ตรงกัน กรุณาระบุใหม่</small>
+          <small>{errorConfirmPassword}</small>
         </div>
         <button type='submit'>Register</button>
       </form>
@@ -70,3 +112,6 @@ root.render(<Form />);
 
 // create : function Form() {}
 // render : <Form/>
+
+// Challenge
+// 4 State
